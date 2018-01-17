cd05f79887f6718f2754c28508039d6d7c0841c0
Sayed Hussaini <seh31628@uga.edu>
2018-01-17 14:05:33 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,102 +1,16 @@
 import React from 'react';
-import { TextInput,StatusBar,StyleSheet, Text, TouchableHighlight, View,Image,Dimensions } from 'react-native';
-import MainInput from './MainInput'
-import zodiacpic from './zodiacpic.jpg';
-import backgroundMain from './backgroundMain.jpg'
-export default class App extends React.Component {
-    constructor(props){
-	super(props);
-	this.state = {
-	    score:'1',
-	    text:'Place holder text'
+import {Text,View} from 'react-native';
 
-	}
-	
-	
-    }    
-    render(){
-      
-	return( 
-	           
-	       <Image source ={backgroundMain} style={styles.backgroundImage}>
-	       <View style={styles.container}>
-	    <StatusBar hidden={true}/>
-	       <Text style={[styles.baseText,styles.titleText]}>Zodiac Teller!</Text>           
-	       <Text style={[styles.baseText,styles.titleText]}>Enter a Birthday: </Text> 
+import {StackNavigator} from 'react-navigation';
+import FirstScreen from './screens/FirstScreen'
+import SecondScreen from './screens/SecondScreen'
 
-	       < MainInput />
-	       <TouchableHighlight
-	       activeOpacity={0.6}
-	       underlayColor={'blue'}
-	       onPress={() => this.setState({score: ++this.state.score})}>
-	       
-	     
-	       <Text style={styles.button}>Submit</Text>
-	       </TouchableHighlight>
-	       <Text>{this.state.score}</Text>
-
-           <Image style={styles.pic} source ={zodiacpic}/>
-	   
-	        
-	   
-	       </View>
-	     </Image>
-		);
- 
-    }
-}
-
-// Flex is important to parent and children relationship.
-// The axis matters too, row vs Colums etc
-
-const styles = StyleSheet.create({
-  container: {
-      flex:1,
-	    flexDirection:'column',     // How content in the flexBox will be oriented
-	    justifyContent:'space-between', // Centering the content of flexBox center
-	    backgroundColor: 'transparent',
-      
-            alignItems: 'center'
-
-  },
-  pic:{
-	    
-	    borderRadius:100,
-	    // resizeMode:''
-	    width: 200, height: 200
-  },
-  backgroundImage: {
-	    flex: 1,
-	    
-	    width: null,
-	     // or 'stretch',
-  },
-
-	  baseText: {
-	      fontFamily: 'Cochin',
-	      
-	  },
-
-
-	  titleText: {
-	      fontSize: 25,
-	      fontWeight: 'bold',
-	      color:'white',
-	      padding: 20,
-	      margin:5,
-	      borderWidth:0
-	  },
-  button:{
-
-            fontSize:30,
-	    color:'white',
-            margin: 10,
-            padding:10,
-            borderWidth:2,
-	    borderColor:'white',
-            borderRadius:10,
-            alignSelf:'stretch',
-            textAlign: 'center'
-  }
-     
+//Navigation for the App Screens
+const Navigation = StackNavigator({
+    
+        First:{screen:FirstScreen},
+        Second:{screen:SecondScreen}
 });
+
+
+export default Navigation;
